96799eaeeb3c619c1183672418cc14094a7e5d69
betobates <rbwestmoreland@gmail.com>
2012-05-14 20:10:30 -0400

    Fixes issue #1

--- js/date.js
+++ js/date.js
@@ -5,7 +5,7 @@ var dateModule = (function () {
     function get() {
         var date = new Date();
         var month = date.getMonth() + 1;
-        var day = date.getDay();
+        var day = date.getDate();
         var year = date.getFullYear();
         return month + '/' + day + '/' + year + ' ' + date.toLocaleTimeString();
     }
